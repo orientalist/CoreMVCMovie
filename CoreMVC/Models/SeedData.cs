b1709440f4f6e67692ecf545a5c04f9fba998db3
SW <x774436248@gmail.com>
2018-11-06 21:48:23 +0800

    Core MVC Movies

---
+++ CoreMVC/Models/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Models
+{
+    public static class SeedData
+    {
+        public static void Initailize(IServiceProvider serviceProvider)
+        {
+            using (var context=new CoreMVCContext(
+                serviceProvider.GetRequiredService<DbContextOptions<CoreMVCContext>>()))
+            {
+                if (context.Movie.Any())
+                    return;
+
+                context.Movie.Add(
+                    new Movie {
+                        Title = "Default Movie",
+                        ReleaseDate = DateTime.Parse("1993-10-31"),
+                        Genre="Comedy",
+                        Price=7.99M
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CoreMVC/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMVC.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CoreMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope=host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<CoreMVCContext>();
+                    context.Database.Migrate();
+                    SeedData.Initailize(services);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error occured seeding the DB.");
+                }
+            }
+            host.Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
